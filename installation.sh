4a32ecb6dea746d3c2fc87571aa98456cd06485a
devika-goes-cloud <217624239+devika-goes-cloud@users.noreply.github.com>
2025-08-10 22:22:03 +0530

    Update installation.sh

--- installation.sh
+++ installation.sh
@@ -17,6 +17,21 @@ cat /var/jenkins_home/secrets/initialAdminPassword
 
 
 
+# Trivy installation
+cat << EOF | sudo tee -a /etc/yum.repos.d/trivy.repo
+[trivy]
+name=Trivy repository
+baseurl=https://aquasecurity.github.io/trivy-repo/rpm/releases/\$basearch/
+gpgcheck=1
+enabled=1
+gpgkey=https://aquasecurity.github.io/trivy-repo/rpm/public.key
+EOF
+sudo yum -y update
+sudo yum -y install trivy
+
+
+
+
 
 git clone https://github.com/devika-goes-cloud/SpringBoot-App-EKS.git
 
