d28e14edc2e9fddaaf11d0dad4debf5e466495f8
devika-goes-cloud <217624239+devika-goes-cloud@users.noreply.github.com>
2025-08-10 21:45:54 +0530

    Update installation.sh

--- installation.sh
+++ installation.sh
@@ -3,12 +3,11 @@ sudo yum install git -y
 sudo yum install java -y
 
 sudo yum install docker -y
-
-
 sudo systemctl enable docker
 sudo systemctl start docker
 docker --version
 sudo docker version
+
 
 docker images
 docker ps
