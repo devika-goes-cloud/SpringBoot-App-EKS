9a92d82d3150ba0ce30cc1b8597279c9a0639270
devika-goes-cloud <217624239+devika-goes-cloud@users.noreply.github.com>
2025-08-10 21:45:25 +0530

    Update installation.sh

--- installation.sh
+++ installation.sh
@@ -10,4 +10,14 @@ sudo systemctl start docker
 docker --version
 sudo docker version
 
+docker images
+docker ps
+docker run -d -p 8081:8080 --name jenkins-container jenkins/jenkins:lts
+docker exec -it jenkins-container bash
+cat /var/jenkins_home/secrets/initialAdminPassword
+
+
+
+
+git clone https://github.com/devika-goes-cloud/SpringBoot-App-EKS.git
 
