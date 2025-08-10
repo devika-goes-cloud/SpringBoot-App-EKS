ea334d06b643f6659a51d53f48466b9e5f7354b7
devika-goes-cloud <217624239+devika-goes-cloud@users.noreply.github.com>
2025-08-10 21:33:31 +0530

    Create installation.sh

---
+++ installation.sh
@@ -0,0 +1,13 @@
+
+sudo yum install git -y
+sudo yum install java -y
+
+sudo yum install docker -y
+
+
+sudo systemctl enable docker
+sudo systemctl start docker
+docker --version
+sudo docker version
+
+
