e34e90e676d31490e090bf7a03522ab0984938dc
devika-goes-cloud <217624239+devika-goes-cloud@users.noreply.github.com>
2025-08-11 17:55:02 +0530

    Update installation.sh

--- installation.sh
+++ installation.sh
@@ -40,8 +40,8 @@ Maven installation:
 ###############################
 cd /opt
 sudo wget https://dlcdn.apache.org/maven/maven-3/3.9.11/binaries/apache-maven-3.9.11-bin.tar.gz
-sudo tar -xzf apache-maven-3.9.10-bin.tar.gz
-sudo mv apache-maven-3.9.10 maven
+sudo tar -xzf apache-maven-3.9.11-bin.tar.gz
+sudo mv apache-maven-3.9.11 maven
 
 
 
