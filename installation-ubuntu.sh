241e850e5a901c09d946a4301c29a9f2cf88532d
devika-goes-cloud <217624239+devika-goes-cloud@users.noreply.github.com>
2025-08-24 20:52:18 +0530

    Update installation-ubuntu.sh

--- installation-ubuntu.sh
+++ installation-ubuntu.sh
@@ -9,7 +9,7 @@ echo "deb [signed-by=/etc/apt/keyrings/jenkins-keyring.asc]" \
   https://pkg.jenkins.io/debian-stable binary/ | sudo tee \
   /etc/apt/sources.list.d/jenkins.list > /dev/null
 sudo apt-get update
-sudo apt-get install jenkins
+sudo apt-get install jenkins -y
 
 sudo systemctl start jenkins
 sudo systemctl enable jenkins
