8b67c34bd9e4dbb79e4fcc6101adf139996bb9d2
devika-goes-cloud <217624239+devika-goes-cloud@users.noreply.github.com>
2025-08-24 17:49:23 +0530

    Create installation-ubuntu.sh

---
+++ installation-ubuntu.sh
@@ -0,0 +1,14 @@
+sudo apt update -y
+sudo apt install openjdk-17-jdk -y
+
+
+sudo wget -O /etc/apt/keyrings/jenkins-keyring.asc \
+  https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key
+echo "deb [signed-by=/etc/apt/keyrings/jenkins-keyring.asc]" \
+  https://pkg.jenkins.io/debian-stable binary/ | sudo tee \
+  /etc/apt/sources.list.d/jenkins.list > /dev/null
+sudo apt-get update
+sudo apt-get install jenkins
+
+sudo systemctl start jenkins
+sudo systemctl enable jenkins
